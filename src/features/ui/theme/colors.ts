dd6a6602d156a9ae2ccd04191a7f262d514c3338
tonycastle <castleantony6@gmail.com>
2022-05-23 09:01:59 +0200

    feat: added theme dir with example palette and colors

---
+++ src/features/ui/theme/colors.ts
@@ -0,0 +1,6 @@
+import { palette } from './palette'
+
+export const colors = {
+  bgPrimary: palette.lightGrey,
+  bgSecondary: palette.white,
+}
---
+++ src/features/ui/theme/palette.ts
@@ -0,0 +1,6 @@
+export const palette = () => ({
+  lightGrey: '#F9F(FB',
+  white: '#FFF',
+  black: '#000',
+  transparent: 'transparennt',
+})
