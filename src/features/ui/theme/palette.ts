b1bf785c67b241e52032d4ee395b320a9653d471
tonycastle <castleantony6@gmail.com>
2022-06-06 15:35:17 +0200

    chore: installed styled componments

--- src/features/ui/theme/palette.ts
+++ src/features/ui/theme/palette.ts
@@ -1,6 +1,21 @@
-export const palette = () => ({
-  lightGrey: '#F9F(FB',
+export const palette = {
+  grey: {
+    100: '#323C46',
+    500: '#949EA8',
+    600: '#A9AEB4',
+    700: '#C9CED3',
+    800: '#D2D6DA',
+    850: '#D9DCE1',
+    900: '#F9F9FB',
+  },
+
   white: '#FFF',
   black: '#000',
   transparent: 'transparent',
-})
+
+  //accents
+  green: '#22D486',
+  greenDarker: '#208078',
+  pink: '#FF4081',
+  pinkDarker: '#E73370',
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
 
 import { HeadDefault } from '~/features/core/components/HeadDefault'
+import { GlobalStyle } from '~/features/ui/theme/GlobalStyle'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <HeadDefault />
       <Component {...pageProps} />
     </>
